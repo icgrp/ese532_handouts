13563a85402ef4b58d3c944e44e89fb5a450170f
Syed Ahmed <stahmed@seas.upenn.edu>
2020-08-30 21:03:39 -0400

    Edit publish script a little more

--- publish.sh
+++ publish.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 jupyter-book clean ese532_handouts/ --all
 jupyter-book build ese532_handouts/
-rm -rf ~/fall2020/html/handouts/*
-cp -r ese532_handouts/_build/html/* ~/fall2020/html/handouts/
+cp -r ese532_handouts/_build/html ~/fall2020/html/handouts
 chmod a+rx ~/fall2020/html/handouts/
 chmod a+r ~/fall2020/html/handouts/index.html
 find ~/fall2020/html/handouts/* -type d -exec chmod a+rx {} +
